abe4df4bc2afe0247bd602b348dfd53496eafc0e
usha <usha.ns@oliveboard.in>
2018-09-27 15:00:24 +0530

    commt after build sucess

--- app/src/main/java/in/aar/aarlibapp/MainActivity.java
+++ app/src/main/java/in/aar/aarlibapp/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+public String apikey = "4f6f09640654e7f5cf7c47633d010558cceee543";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
